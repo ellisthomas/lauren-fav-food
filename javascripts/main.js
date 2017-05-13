4a2fa1fce900824f3618a083ed0ecbabc7298c73
ellisthomas <ellisthomas80@gmail.com>
2017-05-12 23:50:17 -0500

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log("hello");
+var app = angular.module("LauApp", []);
+
+app.controller("ItemCtrl", ($scope) => {
+	
+	$scope.dog = "Woof!";
+	$scope.showListView = true;
+	$scope.items = [
+        {
+          id: 0,
+          food: "pizza",
+        },
+        {
+          food: "steak",
+        },
+        {
+          food: "cheese wiz",
+        }
+      ];
+
+});
